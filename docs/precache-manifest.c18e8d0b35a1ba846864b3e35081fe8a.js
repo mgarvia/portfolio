d7ffeaeb941d93ae780ae268ba66f809e4385d11
mgarvia <maria.garvia.soler@gmail.com>
2020-06-09 14:06:47 +0200

    translated to spanish

---
+++ docs/precache-manifest.c18e8d0b35a1ba846864b3e35081fe8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e23935edbca3fc7e555a6c9711e195",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d4f00d48fbb3544afb3",
+    "url": "./static/css/main.53f994ea.chunk.css"
+  },
+  {
+    "revision": "f7101933beea4c0c42b8",
+    "url": "./static/js/2.e9edfd30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e9edfd30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4f00d48fbb3544afb3",
+    "url": "./static/js/main.2b052f92.chunk.js"
+  },
+  {
+    "revision": "a89f26a9cb8e999076fe",
+    "url": "./static/js/runtime-main.6c008890.js"
+  }
+]);
