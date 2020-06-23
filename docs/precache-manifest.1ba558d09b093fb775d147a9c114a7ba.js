0739da8ee6d1662aadacf2721cfb0a7c93cd16dc
mgarvia <maria.garvia.soler@gmail.com>
2020-06-23 16:12:10 +0200

    docs

---
+++ docs/precache-manifest.1ba558d09b093fb775d147a9c114a7ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66cf030c196808e3ad42c0b070ff924",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d6535c32265f28c4eea",
+    "url": "./static/css/main.928b4189.chunk.css"
+  },
+  {
+    "revision": "f7101933beea4c0c42b8",
+    "url": "./static/js/2.e9edfd30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e9edfd30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d6535c32265f28c4eea",
+    "url": "./static/js/main.2df10d11.chunk.js"
+  },
+  {
+    "revision": "a89f26a9cb8e999076fe",
+    "url": "./static/js/runtime-main.6c008890.js"
+  }
+]);
